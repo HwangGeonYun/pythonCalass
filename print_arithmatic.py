ab0ea12ca5131f5ae8ad080ee415267808797b6b
HwangGeonYun <ghkdwjdgns15@gmail.com>
2023-03-15 10:07:17 +0900

    class01

---
+++ print_arithmatic.py
@@ -0,0 +1,7 @@
+def print_arithmatic(a, b):
+    print(str(a), '+', str(b), '=', str(a+b) + '\n'+
+           str(a), '-', str(b), '=', str(a-b) + '\n'+
+           str(a), '*', str(b), '=', str(a*b) + '\n'+
+           str(a), '/', str(b), '=', str(a/b) + '\n')
+    
+print_arithmatic(50, 30)
